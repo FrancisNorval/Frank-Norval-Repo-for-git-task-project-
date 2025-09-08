7155ee6411dcc5da1e7db0ffd2645beda69498a0
Frank Norval <franknorval2019@gmail.com>
2025-09-08 12:07:46 +0200

    Improved the comments to better describe what is being done in the code

--- award.py
+++ award.py
@@ -1,10 +1,10 @@
-# Naming the variables according to the activity  and asking the user for input
+# Naming the variables according to the activity the candidate is participating in and asking the user for input
 candidate_swimming_time=int(input("Please enter the swimming time in minutes: "))
 candidate_cycling_time=int(input("Please enter the cycling time in minutes: "))
 candidate_running_time=int(input("Please enter the running time in minutes: "))
 Total_time= candidate_swimming_time + candidate_cycling_time + candidate_running_time
 print(f"Total time taken: {Total_time} minutes")
-#Calculating the total time of the to determine what colours they will qualify for 
+#Calculating the total time of the to determine what colours the candidate will qualify for 
 Qualifying_time= 100 
 if Total_time <= Qualifying_time:
     award = "provicial colours"
@@ -14,5 +14,5 @@ elif Total_time <= Qualifying_time + 10:
     award = "provincial scroll"
 else:
    award = "No award"
-#Printing out what colours the has recieved
+#Printing out what colours the candidate has recieved after completing the calculation
 print(f"Award recieved: {award}")
